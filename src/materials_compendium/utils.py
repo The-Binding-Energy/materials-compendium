42e02f634380c6571a83b6780226de0c0d88a070
Eliott Ramoisiaux <eliott@thebindingenergy.com>
2025-02-10 14:14:01 +0100

    Improve from_name error message

--- src/materials_compendium/utils.py
+++ src/materials_compendium/utils.py
@@ -852,7 +852,7 @@ class Material:
             suggestions = "\n".join(matched_materials)
             message = f"Material '{material_name}' not found. Did you mean:\n{suggestions}. Otherwise, please check the online reference 'https://mcnp.lanl.gov/pdf_files/TechReport_2021_PNNL_PNNL-15870Rev.2_DetwilerMcConnEtAl.pdf'"
         else:
-            message = fr"Material '{material_name}' not found in the data. Please check the online reference 'https://mcnp.lanl.gov/pdf_files/TechReport_2021_PNNL_PNNL-15870Rev.2_DetwilerMcConnEtAl.pdf'"
+            message = fr"Material '{material_name}' not found in the data. Please check the online reference 'https://mcnp.lanl.gov/pdf_files/TechReport_2021_PNNL_PNNL-15870Rev.2_DetwilerMcConnEtAl.pdf'. You can copy-paste the name any Material from the Compendium, please just delete any commas from the original name."
         return None, message
 
     @classmethod
